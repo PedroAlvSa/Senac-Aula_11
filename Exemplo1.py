1d6a0fd8c8ecd6d8fb601b2f81ebecd2a4fc6563
Pedro <86210318+PedroAlvSa@users.noreply.github.com>
2026-01-06 19:44:00 -0300

    Add files via upload

---
+++ Exemplo1.py
@@ -0,0 +1,19 @@
+#Bibliotecas
+import random
+
+# random.randit -> Números inteiros
+n = random.randint(1, 10)
+m = random.randint(1, 10)
+print(n, m)
+
+# ---------------------------------------------
+# Gerar números decimais
+n_decimal = random.uniform(1, 10)
+numero_decimal = round(n_decimal, 3) # Arredonda
+print(f'{n_decimal:.2f}') # Formata para duas casas
+print(numero_decimal)
+
+# ---------------------------------------------
+# Gerar números aleatórios dentro de uma lista
+lst_numeros = [random.randint(1 ,10) for num in range(5)]
+print(lst_numeros)
